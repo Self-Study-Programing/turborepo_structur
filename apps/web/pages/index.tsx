03e48ad72da35e90b0d14e0579b1fb755cd13e08
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-11-06 17:21:58 +0900

    storybook

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,18 @@
-import { Button } from "ui";
+import { Button, Header } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <Header
+        onCreateAccount={() => {}}
+        onLogin={function noRefCheck() {}}
+        onLogout={function noRefCheck() {}}
+        user={{
+          name: "Jane Doe",
+        }}
+      />
     </div>
   );
 }
